4d8a922fa3bb323552b7788887e6f2cb826b53c2
MilagrosHassan <milagroshassan@gmail.com>
2023-07-13 18:02:28 +0200

    tweet component

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,21 +1,57 @@
 import { View, Text, Image, StyleSheet } from "react-native";
 
 import { TweetType } from "../types";
+import { Entypo, EvilIcons } from '@expo/vector-icons'; 
+
+type IconButtonProps = {
+  icon: React.ComponentProps<typeof EvilIcons>["name"];
+  text?: string | number;
+};
+
+const IconButton = ({ icon, text }: IconButtonProps) => {
+  return (
+    <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <EvilIcons name={icon} size={24} color="grey" />
+
+      <Text style={{ fontSize: 12, color: "grey" }}>{text} </Text>
+    </View>
+  );
+};
 
 type TweetProps = {
-    tweet: TweetType;
-}
+  tweet: TweetType;
+};
 
 
 
-const Tweet = ({ tweet } : TweetProps) => {
+const Tweet = ({ tweet }: TweetProps) => {
   return (
     <View style={styles.container}>
       <Image src={tweet.user.image} style={styles.userImage} />
 
       <View style={styles.MainContainer}>
-        <Text style={styles.name}> {tweet.user.name}</Text>
+        <View style={{ flexDirection: "row" }}>
+          <Text style={styles.name}>{tweet.user.name}</Text>
+          <Text style={styles.username}>{tweet.user.username} • 2h</Text>
+          <Entypo
+            name="dots-three-horizontal"
+            size={16}
+            color="grey"
+            style={{ marginLeft: "auto" }}
+          />
+        </View>
+
         <Text style={styles.content}>{tweet.content}</Text>
+
+        {tweet.image && <Image src={tweet.image} style={styles.image} />}
+
+        <View style={styles.footer}>
+          <IconButton icon="comment" text={tweet.numberOfComments} />
+          <IconButton icon="retweet" text={tweet.numberOfRetweets} />
+          <IconButton icon="heart" text={tweet.numberOfLikes} />
+          <IconButton icon="chart" text={tweet.impressions || 0} />
+          <IconButton icon="share-apple" />
+        </View>
       </View>
     </View>
   );
@@ -42,8 +78,25 @@ const styles = StyleSheet.create({
   name: {
     fontWeight: "bold",
   },
+  username:{
+    color:"grey",
+    marginLeft:5,
+  },
   content: {
     lineHeight: 20,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 16 / 9,
+    marginVertical:10,
+    borderRadius: 15, 
+  },
+
+  //footer
+  footer:{
+    flexDirection: "row",
+    marginVertical:5,
+    justifyContent: "space-between",
   },
 });
 
