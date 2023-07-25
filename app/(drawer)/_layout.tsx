ce0e3fb62ce5801793284c254dbd75f4ecfe8a48
MilagrosHassan <milagroshassan@gmail.com>
2023-07-26 01:07:45 +0200

    Update_layout.tsx

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,6 +1,7 @@
 import { withLayoutContext } from "expo-router";
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from "@react-navigation/drawer";
-import {Image, Text, StyleSheet, ScrollView, ScrollViewProps} from "react-native"
+import {Image, Text, StyleSheet, View ,ScrollView, ScrollViewProps} from "react-native"
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const DrawerNavigator = createDrawerNavigator().Navigator;
 
@@ -14,16 +15,40 @@ const user = {
   username: "jlowatson",
   name: "Jason",
   image: "https://randomuser.me/api/portraits/men/68.jpg",
+  following:  337 ,
+  followers: 354 ,
 };
 
 function CustomDrawerContent(props) {
     return (
       <DrawerContentScrollView style={styles.container} {...props}>
-        <Image src= "https://randomuser.me/api/portraits/men/33.jpg" 
-    style={{ width: 40, aspectRatio: 1, borderRadius: 40, marginLeft:15}}
-    />
-    <Text style= {styles.name} > {user.name} </Text>
-    <Text style= {styles.username}> @{user.username} </Text>
+        
+        <View style={styles.TopContainer}>
+          <View style={{ flexDirection: "row" }}> 
+                <Image src= "https://randomuser.me/api/portraits/men/33.jpg" 
+                  style={{ 
+                    width: 40,
+                    aspectRatio: 1,
+                    borderRadius: 40,
+                    marginLeft:5}}
+                  />
+                <MaterialCommunityIcons
+                  name="dots-vertical-circle-outline" 
+                  size={24} color="black" 
+                  style={{ marginLeft: "auto" }} />
+          </View>
+      
+          <Text style= {styles.name} > {user.name} </Text>
+          <Text style= {styles.username}> @{user.username} </Text>
+
+        <View style= {{flexDirection:"row", marginTop:10,}}>  
+          <Text > {user.following} </Text>
+          <Text style={{color:"grey"}}>Following </Text>
+          <Text> {user.followers} </Text>
+          <Text style={{color:"grey"}}>Followers</Text>
+        </View>
+        
+       </View>
         <DrawerItemList {...props} />
       </DrawerContentScrollView>
     );
@@ -31,20 +56,27 @@ function CustomDrawerContent(props) {
 
 
 const styles = StyleSheet.create({
+  TopContainer:{
+    marginBottom:10, 
+    padding: 15, 
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: "lightgrey",
+
+  },
+
   container:{
     marginLeft:5,
-
   },
 
 name:{
   fontWeight: "bold",
   fontSize: 20,
-  marginLeft: 10,
+  
 },
 
 username:{
   color: "grey",
-  marginLeft: 10,
+  
 }
 
 
