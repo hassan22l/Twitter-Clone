a2412cbe30e012ef9f0f9de8cfd96a251ffdb4f5
MilagrosHassan <milagroshassan@gmail.com>
2023-07-25 23:52:00 +0200

    Home options

--- app/(drawer)/(tabs)/_layout.tsx
+++ app/(drawer)/(tabs)/_layout.tsx
@@ -1,19 +1,15 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link, Tabs, useNavigation } from 'expo-router';
 import { Pressable, useColorScheme, Image } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import Colors from '../../../constants/Colors';
 
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
-
 function AvatarHeader (){
   const navigation = useNavigation();
   return(
@@ -31,12 +27,15 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-      }}>
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+      }}
+    >
       <Tabs.Screen
-        name="index" 
+        name="index"
         options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="home" size={25} color="black" />
+          ),
           headerRight: () => (
             <Link href="/modal" asChild>
               <Pressable>
@@ -44,21 +43,39 @@ export default function TabLayout() {
                   <FontAwesome
                     name="info-circle"
                     size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
+                    color={Colors[colorScheme ?? "light"].text}
                     style={{ marginLeft: 15, opacity: pressed ? 0.5 : 1 }}
                   />
                 )}
               </Pressable>
             </Link>
           ),
-          headerLeft: () => <AvatarHeader/>
+          headerLeft: () => <AvatarHeader />,
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="search"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <Feather name="search" size={25} color="black" />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="notifications"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Feather name="bell" size={25} color="black" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="chat"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="email-outline" size={25} color="black" />
+          ),
         }}
       />
     </Tabs>
--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,22 +1,55 @@
 import { withLayoutContext } from "expo-router";
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from "@react-navigation/drawer";
-import { Text } from "../../components/Themed";
+import {Image, Text, StyleSheet, ScrollView, ScrollViewProps} from "react-native"
+
 const DrawerNavigator = createDrawerNavigator().Navigator;
 
 const Drawer = withLayoutContext(DrawerNavigator);
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
   initialRouteName: "(tabs)",
+};
+
+const user = {
+  id: "u1",
+  username: "jlowatson",
+  name: "Jason",
+  image: "https://randomuser.me/api/portraits/men/68.jpg",
 };
 
 function CustomDrawerContent(props) {
     return (
-      <DrawerContentScrollView {...props}>
-        <Text style={{alignSelf: "center", fontSize: 20}}> Milagros</Text>
+      <DrawerContentScrollView style={styles.container} {...props}>
+        <Image src= "https://randomuser.me/api/portraits/men/33.jpg" 
+    style={{ width: 40, aspectRatio: 1, borderRadius: 40, marginLeft:15}}
+    />
+    <Text style= {styles.name} > {user.name} </Text>
+    <Text style= {styles.username}> @{user.username} </Text>
         <DrawerItemList {...props} />
       </DrawerContentScrollView>
     );
 }
+
+
+const styles = StyleSheet.create({
+  container:{
+    marginLeft:5,
+
+  },
+
+name:{
+  fontWeight: "bold",
+  fontSize: 20,
+  marginLeft: 10,
+},
+
+username:{
+  color: "grey",
+  marginLeft: 10,
+}
+
+
+})
+
 
 export default function DrawerLayout() {
   return (
